6921c811d496875010a2a66c095177677b334eef
Yoshihara, Sean Tsubasa <vcsean3@gmail.com>
2025-02-12 02:55:34 -0500

    Card studying

--- src/components/ui/CaseStudyCard.tsx
+++ src/components/ui/CaseStudyCard.tsx
@@ -29,10 +29,10 @@ export default function CaseStudyCard(
 
     return (
         <div className="flex items-center justify-center h-screen sticky top-0">
-            <motion.div className="space-y-8 rounded-xl relative lg:h-[550px]"
+            <motion.div className="space-y-8 rounded-md relative lg:h-[550px]"
                  style={{backgroundColor: color, scale: scale, top: `calc(-5vh + ${index * 40}px)`}}
             >
-                <div className="grid grid-cols-1 md:grid-cols-[1fr,1px,1fr] gap-8 items-center p-4 h-full">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center p-4 h-full">
                     <div className="space-y-6 p-6 md:p-8">
                         <div className="space-y-6">
                             <p className="text-sm font-medium tracking-wider text-neutral-950">ITEM {number}</p>
@@ -49,9 +49,8 @@ export default function CaseStudyCard(
                             </div>
                         </div>
                     </div>
-                    <Separator orientation="vertical" className="hidden md:block h-full"/>
                     <div
-                        className="relative aspect-[4/3] overflow-hidden group transition-all duration-500 ease-in-out rounded-xl m-4">
+                        className="relative aspect-[4/3] overflow-hidden group transition-all duration-500 ease-in-out rounded-md m-4">
                         <Link href={siteUrl || "/"} target={"_blank"} rel="noopener noreferrer"
                               className={!siteUrl ? 'pointer-events-none' : ''}
                               aria-disabled={!siteUrl}
